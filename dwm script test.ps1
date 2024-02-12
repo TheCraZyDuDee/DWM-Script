56dcab7f87ababe05f502eda23bc28fbf6065305
crusty <83429736+crustySenpai@users.noreply.github.com>
2024-02-12 04:24:51 +0100

    Update dwm script test.ps1
    - Added Windows Version Check
    - reworked the gui a little
    - added more logging
    - and more

--- dwm script test.ps1
+++ dwm script test.ps1
@@ -1,4 +1,4 @@
-#Silly Script made by crusty & OpenAI's GPT-3
+# Script made by crusty & OpenAI's GPT-3
 # Configuration
 $LogPath = "$PSScriptRoot\dwm_script_log.txt"
 $PsSuspendPath = "$PSScriptRoot\Tools\PSSuspend\PsSuspend.exe"
@@ -12,88 +12,45 @@ function Log-Message {
     Add-content -Path $LogPath -Value $Message
 }
 
-# Function to check if PsSuspend is already downloaded
-function Is-PsSuspendDownloaded {
-    return Test-Path -Path $PsSuspendPath
-}
-
-# Function to download PsSuspend
-function Download-PsSuspend {
-    try {
-        Log-Message "Downloading PsSuspend..."
-        $null = New-Item -Path (Split-Path -Path $PsSuspendPath) -ItemType Directory -Force
-        Invoke-WebRequest -Uri $PsSuspendDownloadURL -OutFile $PsSuspendPath -ErrorAction Stop
-        Log-Message "PsSuspend downloaded successfully"
-        return $true
+# Function to display a MessageBox
+function Show-MessageBox {
+    param(
+        [string]$Message,
+        [string]$Title,
+        [string]$Icon,
+        [string[]]$Buttons
+    )
+    Add-Type -AssemblyName System.Windows.Forms
+    $buttonEnum = [System.Windows.Forms.MessageBoxButtons]::OK
+    switch ($Buttons) {
+        "YesNo" { $buttonEnum = [System.Windows.Forms.MessageBoxButtons]::YesNo }
+        "OK" { $buttonEnum = [System.Windows.Forms.MessageBoxButtons]::OK }
+        "OKCancel" { $buttonEnum = [System.Windows.Forms.MessageBoxButtons]::OKCancel }
+        "AbortRetryIgnore" { $buttonEnum = [System.Windows.Forms.MessageBoxButtons]::AbortRetryIgnore }
+        "RetryCancel" { $buttonEnum = [System.Windows.Forms.MessageBoxButtons]::RetryCancel }
+        "YesNoCancel" { $buttonEnum = [System.Windows.Forms.MessageBoxButtons]::YesNoCancel }
+        default { $buttonEnum = [System.Windows.Forms.MessageBoxButtons]::OK }
     }
-    catch {
-        Log-Message "Failed to download PsSuspend: $_"
-        return $false
-    }
+    $result = [System.Windows.Forms.MessageBox]::Show($Message, $Title, $buttonEnum, $Icon)
+    return $result
 }
 
-# Check if running as admin
-if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    $scriptPath = $MyInvocation.MyCommand.Path
-    Start-Process powershell.exe -Verb RunAs -ArgumentList "-WindowStyle Hidden -File `"$scriptPath`""
-    exit
-}
-
-# Check if PsSuspend is already downloaded
-if (-not (Is-PsSuspendDownloaded)) {
-    # Prompt to download PsSuspend
-    $downloadChoice = [System.Windows.Forms.MessageBox]::Show("PsSuspend is required to run this script. Do you want to download it now?", "Download PsSuspend", "YesNo", "Warning")
-
-    if ($downloadChoice -eq "Yes") {
-        if (-not (Download-PsSuspend)) {
-            Log-Message "Failed to download PsSuspend. Exiting script."
-            [System.Windows.Forms.MessageBox]::Show("Failed to download PsSuspend. Exiting script.", "Error", "OK", "Error")
-            exit
-        }
-    } else {
-        Log-Message "PsSuspend is required to run this script. Exiting."
-        [System.Windows.Forms.MessageBox]::Show("PsSuspend is required to run this script. Exiting.", "Error", "OK", "Error")
-        exit
-    }
-}
+# Log start of the script
+Log-Message "Starting the script"
 
 # Function to check if DWM is enabled
 function Is-DWMEnabled {
     return (Get-Process -Name "dwm" -ErrorAction SilentlyContinue) -ne $null
 }
 
-# Function to suspend winlogon
-function Suspend-Winlogon {
-    try {
-        Start-Process -FilePath "$PsSuspendPath" -ArgumentList "winlogon.exe" -NoNewWindow -Wait -ErrorAction Stop
-        Log-Message "Winlogon suspended successfully"
-        return $true
-    }
-    catch {
-        Log-Message "Failed to suspend Winlogon: $_"
-        return $false
-    }
-}
-
-# Function to resume winlogon
-function Resume-Winlogon {
-    try {
-        Start-Process -FilePath "$PsSuspendPath" -ArgumentList "-r winlogon.exe" -NoNewWindow -Wait -ErrorAction Stop
-        Log-Message "Winlogon resumed successfully"
-        return $true
-    }
-    catch {
-        Log-Message "Failed to resume Winlogon: $_"
-        return $false
-    }
-}
+# Log whether DWM is enabled
+Log-Message "Is DWM enabled? $(Is-DWMEnabled)"
 
 # Function to disable DWM
 function Disable-DWM {
     try {
-        if (-not (Suspend-Winlogon)) {
-            throw "Failed to suspend Winlogon"
-        }
+        Start-Process -FilePath "$PsSuspendPath" -ArgumentList "winlogon.exe" -NoNewWindow -Wait -ErrorAction Stop
+        Log-Message "Winlogon suspended successfully"
         
         $processesToStop = @("wallpaper32", "explorer", "dwm", "SearchApp", "TextInputHost", "StartMenuExperienceHost", "ShellExperienceHost")
         foreach ($processName in $processesToStop) {
@@ -112,9 +69,7 @@ function Disable-DWM {
 # Function to enable DWM
 function Enable-DWM {
     try {
-        if (-not (Resume-Winlogon)) {
-            throw "Failed to resume Winlogon"
-        }
+        Start-Process -FilePath "$PsSuspendPath" -ArgumentList "-r winlogon.exe" -NoNewWindow -Wait -ErrorAction Stop
         
         if (-not (Get-Process -Name "explorer" -ErrorAction SilentlyContinue)) {
             Start-Process -FilePath "explorer.exe" -ErrorAction Stop
@@ -156,20 +111,102 @@ function Open-GitHubLink {
     }
 }
 
+# Function to update DWM status label
+function Update-DWMStatusLabel {
+    if (Is-DWMEnabled) {
+        $dwmStatusLabel.Text = "DWM Status: Enabled"
+    } else {
+        $dwmStatusLabel.Text = "DWM Status: Disabled"
+    }
+}
+
+# Function to download PsSuspend
+function Download-PsSuspend {
+    try {
+        Log-Message "Downloading PsSuspend..."
+        $null = New-Item -Path (Split-Path -Path $PsSuspendPath) -ItemType Directory -Force
+        Invoke-WebRequest -Uri $PsSuspendDownloadURL -OutFile $PsSuspendPath -ErrorAction Stop
+        Log-Message "PsSuspend downloaded successfully"
+        return $true
+    }
+    catch {
+        Log-Message "Failed to download PsSuspend: $_"
+        return $false
+    }
+}
+
+# Function to check if PsSuspend is already downloaded
+function Is-PsSuspendDownloaded {
+    return Test-Path -Path $PsSuspendPath
+}
+
+# Check if running as admin
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if (-not $isAdmin) {
+    $scriptPath = $MyInvocation.MyCommand.Path
+    Start-Process powershell.exe -Verb RunAs -ArgumentList "-WindowStyle Hidden -File `"$scriptPath`""
+    exit
+}
+
+# Function to get Windows version and build number
+function Get-WindowsVersionAndBuild {
+    $os = Get-WmiObject -Class Win32_OperatingSystem
+    $majorVersion = $os.Version.Split('.')[0]
+    $minorVersion = $os.Version.Split('.')[1]
+    $buildNumber = $os.BuildNumber
+    return $majorVersion, $minorVersion, $buildNumber
+}
+
+# Check if OS is supported
+$majorVersion, $minorVersion, $buildNumber = Get-WindowsVersionAndBuild
+
+Log-Message "Detected OS Version: $majorVersion.$minorVersion (Build: $buildNumber)"
+
+if ($majorVersion -eq 10 -and $buildNumber -lt 22000) {
+    Log-Message "Supported OS detected: Windows 10"
+} elseif ($majorVersion -eq 10 -and $buildNumber -ge 22000) {
+    Log-Message "Unsupported OS detected: Windows 11"
+    Show-MessageBox "OS not Supported!" "Error" "Error" @("OK")
+    exit
+} else {
+    Log-Message "Unsupported OS detected: $majorVersion.$minorVersion (Build: $buildNumber)"
+    Show-MessageBox "OS not Supported!" "Error" "Error" @("OK")
+    exit
+}
+
+# Check if PsSuspend is already downloaded
+if (-not (Is-PsSuspendDownloaded)) {
+    Log-Message "PsSuspend is required to run this script. Prompting to download."
+    # Prompt to download PsSuspend
+    $downloadChoice = Show-MessageBox "PsSuspend is required to run this script. Do you want to download it now?" "Download PsSuspend" "Warning" @("YesNo")
+
+    if ($downloadChoice -eq "Yes") {
+        if (-not (Download-PsSuspend)) {
+            Log-Message "Failed to download PsSuspend. Exiting script."
+            Show-MessageBox "Failed to download PsSuspend. Exiting script." "Error" "Error" @("OK")
+            exit
+        }
+    } else {
+        Log-Message "PsSuspend is required to run this script. Exiting."
+        Show-MessageBox "PsSuspend is required to run this script. Exiting." "Error" "Error" @("OK")
+        exit
+    }
+}
+
 # Main script
 try {
     # Display GUI
     Add-Type -AssemblyName System.Windows.Forms
     $form = New-Object System.Windows.Forms.Form
     $form.Text = "DWM Script"
-    $form.Size = New-Object System.Drawing.Size(300,200)
+    $form.Size = New-Object System.Drawing.Size(300,160)
     $form.StartPosition = "CenterScreen"
     $form.FormBorderStyle = 'FixedDialog'
     $form.BackColor = [System.Drawing.ColorTranslator]::FromHtml("#00245c")  # Set background color
 
     # GitHub button
     $githubButton = New-Object System.Windows.Forms.Button
-    $githubButton.Location = New-Object System.Drawing.Point(150,120)
+    $githubButton.Location = New-Object System.Drawing.Point(150,80)
     $githubButton.Size = New-Object System.Drawing.Size(100,30)
     $githubButton.Text = "Github Page"
     $githubButton.BackColor = [System.Drawing.Color]::White
@@ -178,47 +215,61 @@ try {
     })
     $form.Controls.Add($githubButton)
 
+    # Label to display DWM status
+    $dwmStatusLabel = New-Object System.Windows.Forms.Label
+    $dwmStatusLabel.Location = New-Object System.Drawing.Point(150,10)  # Adjusted position
+    $dwmStatusLabel.Size = New-Object System.Drawing.Size(260,20)
+    $dwmStatusLabel.ForeColor = [System.Drawing.Color]::White
+    $form.Controls.Add($dwmStatusLabel)
+
     # Welcome message
     $welcomeLabel = New-Object System.Windows.Forms.Label
-    $welcomeLabel.Location = New-Object System.Drawing.Point(20,40)
+    $welcomeLabel.Location = New-Object System.Drawing.Point(30,10)
     $welcomeLabel.Size = New-Object System.Drawing.Size(260,20)
     $welcomeLabel.Text = "Welcome, $($env:USERNAME)!"
     $welcomeLabel.ForeColor = [System.Drawing.Color]::White
     $form.Controls.Add($welcomeLabel)
 
+    # Update DWM status label when form loads
+    $form.Add_Load({
+        Update-DWMStatusLabel
+    })
+
     # Disable DWM button
     $disableButton = New-Object System.Windows.Forms.Button
-    $disableButton.Location = New-Object System.Drawing.Point(30,70)
+    $disableButton.Location = New-Object System.Drawing.Point(30,35)  # Adjusted position
     $disableButton.Size = New-Object System.Drawing.Size(100,30)
     $disableButton.Text = "Disable DWM"
     $disableButton.BackColor = [System.Drawing.Color]::White
     $disableButton.Add_Click({ 
         if (Disable-DWM) {
-            [System.Windows.Forms.MessageBox]::Show("DWM disabled successfully", "Success", "OK", "Information")
+            Show-MessageBox "DWM disabled successfully" "Success" "Information" @("OK")
+            Update-DWMStatusLabel  # Update label text
         } else {
-            [System.Windows.Forms.MessageBox]::Show("Failed to disable DWM", "Error", "OK", "Error")
+            Show-MessageBox "Failed to disable DWM" "Error" "Error" @("OK")
         }
     })
     $form.Controls.Add($disableButton)
 
     # Enable DWM button
     $enableButton = New-Object System.Windows.Forms.Button
-    $enableButton.Location = New-Object System.Drawing.Point(150,70)
+    $enableButton.Location = New-Object System.Drawing.Point(150,35)  # Adjusted position
     $enableButton.Size = New-Object System.Drawing.Size(100,30)
     $enableButton.Text = "Enable DWM"
     $enableButton.BackColor = [System.Drawing.Color]::White
     $enableButton.Add_Click({ 
         if (Enable-DWM) {
-            [System.Windows.Forms.MessageBox]::Show("DWM enabled successfully", "Success", "OK", "Information")
+            Show-MessageBox "DWM enabled successfully" "Success" "Information" @("OK")
+            Update-DWMStatusLabel  # Update label text
         } else {
-            [System.Windows.Forms.MessageBox]::Show("Failed to enable DWM", "Error", "OK", "Error")
+            Show-MessageBox "Failed to enable DWM" "Error" "Error" @("OK")
         }
     })
     $form.Controls.Add($enableButton)
 
     # Open Task Manager button
     $taskManagerButton = New-Object System.Windows.Forms.Button
-    $taskManagerButton.Location = New-Object System.Drawing.Point(30,120)
+    $taskManagerButton.Location = New-Object System.Drawing.Point(30,80)  # Adjusted position
     $taskManagerButton.Size = New-Object System.Drawing.Size(100,30)
     $taskManagerButton.Text = "Task Manager"
     $taskManagerButton.BackColor = [System.Drawing.Color]::White
@@ -232,7 +283,7 @@ try {
 }
 catch {
     Log-Message "An error occurred: $_"
-    [System.Windows.Forms.MessageBox]::Show("An error occurred. Please check the log for details.", "Error", "OK", "Error")
+    Show-MessageBox "An error occurred. Please check the log for details." "Error" "Error" @("OK")
 }
 finally {
     $form.Dispose()
